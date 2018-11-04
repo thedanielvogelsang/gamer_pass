5ad6ea988a634d601799998e2af442aa5ca581e9
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-11-04 13:03:55 -0700

    compiles for production and changes root to /

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'gamerpass', to: "gamerpass#landing"
+  get '/', to: "gamerpass#landing"
   post 'user-signup', to: "gamerpass#signup"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
